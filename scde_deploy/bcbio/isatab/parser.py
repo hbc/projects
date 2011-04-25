8409593950ef78779023a0ac7e839bc7898a2dfe
chapmanb <chapmanb@50mail.com>
2011-04-25 16:17:15 -0400

    Initial parsing for Study/Array files with associated metadata

--- scde_deploy/bcbio/isatab/parser.py
+++ scde_deploy/bcbio/isatab/parser.py
@@ -8,6 +8,7 @@ from __future__ import with_statement
 import os
 import csv
 import glob
+import collections
 
 def parse(isatab_ref):
     """Entry point to parse an ISA-Tab directory.
@@ -114,23 +115,125 @@ class InvestigationParser:
         return out, line
 
 class StudyAssayParser:
-    """Parse row oriented metadata about study souress and assay samples.
+    """Parse row oriented metadata associated with study and assay samples.
+
+    This currently does not attempt to be complete, but rather to extract the
+    most useful information (in my biased opinion) and represent it simply
+    in the record objects.
+
+    This is coded generally, so can be expanded to more cases. It is biased
+    towards microarray and next-gen sequencing data.
     """
     def __init__(self, base_file):
         self._dir = os.path.dirname(base_file)
+        self._col_quals = ("Parameter Value", "Performer", "Date", "Unit",
+                           "Term Accession Number", "Term Source REF")
+        self._col_types = {"attribute": ("Characteristics", "Factor Type",
+                                         "Comment", "Label", "Material Type"),
+                           "node" : ("Sample Name", "Source Name", "Image File",
+                                     "Raw Data File", "Derived Data File"),
+                           "node_assay" : ("Extract Name", "Labeled Extract Name",
+                                           "Assay Name", "Data Transformat Name",
+                                           "Normalization Name"),
+                           "processing": ("Protocol REF")}
+        self._synonyms = {"Array Data File" : "Raw Data File",
+                          "Derived Array Data File" : "Derived Data File",
+                          "Hybridization Assay Name": "Assay Name",
+                          "Derived Array Data Matrix File": "Raw Data File",
+                          "Raw Spectral Data File": "Raw Data File",
+                          "Derived Spectral Data File": "Derived Data File"}
 
     def parse(self, rec):
         """Retrieve row data from files associated with the ISATabRecord.
         """
         for study in rec.studies:
-            source_data = self._parse_study(study.metadata["Study File Name"])
+            source_data = self._parse_study(study.metadata["Study File Name"],
+                                            "Sample Name")
+            for assay in study.assays:
+                assay_data = self._parse_study(assay["Study Assay File Name"],
+                                               "Raw Data File")
         return rec
 
-    def _parse_study(self, fname):
+    def _parse_study(self, fname, node_type):
+        """Parse study or assay row oriented file around the supplied base node.
+        """
+        nodes = {}
         with open(os.path.join(self._dir, fname)) as in_handle:
             reader = csv.reader(in_handle, dialect="excel-tab")
+            header = self._swap_synonyms(reader.next())
+            hgroups = self._collapse_header(header)
+            htypes = self._characterize_header(header, hgroups)
+            name_index = header.index(node_type)
             for line in reader:
-                print line
+                name = line[name_index]
+                try:
+                    node = nodes[name]
+                except KeyError:
+                    node = NodeRecord(name, node_type)
+                attrs = self._line_keyvals(line, header, hgroups, htypes)
+                print attrs
+                for i, oi in enumerate(g[0] for g in hgroups):
+                    print htypes[i], header[oi], line[oi]
+                print
+                break
+        return nodes.values()
+
+    def _line_keyvals(self, line, header, hgroups, htypes):
+        out = collections.defaultdict(list)
+        out = self._line_by_type(line, header, hgroups, htypes, out, "attribute")
+        out = self._line_by_type(line, header, hgroups, htypes, out, "processing",
+                                 self._extract_protocol_info)
+        out = self._line_by_type(line, header, hgroups, htypes, out, "node")
+        return dict(out)
+
+    def _line_by_type(self, line, header, hgroups, htypes, out, want_type,
+                      collapse_quals_fn = None):
+        """Parse out key value pairs for line information based on a group of values.
+        """
+        for index, htype in ((i, t) for i, t in enumerate(htypes) if t == want_type):
+            col = hgroups[index][0]
+            key = header[col]
+            if key.find("[") >= 0:
+                key = key.replace("]", "").split("[")[-1]
+            if collapse_quals_fn:
+                val = collapse_quals_fn(line, header, hgroups[index])
+            else:
+                val = line[col]
+            out[key].append(val)
+        return out
+
+    def _extract_protocol_info(self, line, header, indexes):
+        print indexes
+        for i in indexes:
+            print header[i], line[i]
+        print
+
+    def _characterize_header(self, header, hgroups):
+        """Characterize header groups into different data types.
+        """
+        out = []
+        for h in [header[g[0]] for g in hgroups]:
+            this_ctype = None
+            for ctype, names in self._col_types.iteritems():
+                if h.startswith(names):
+                    this_ctype = ctype
+                    break
+            out.append(this_ctype)
+        return out
+
+    def _collapse_header(self, header):
+        """Combine header columns into related groups.
+        """
+        out = []
+        for i, h in enumerate(header):
+            if h.startswith(self._col_quals):
+                out[-1].append(i)
+            else:
+                out.append([i])
+        return out
+
+    def _swap_synonyms(self, header):
+        return [self._synonyms.get(h, h) for h in header]
 
 class ISATabRecord:
     """Represent ISA-Tab metadata in structured format.
@@ -162,3 +265,11 @@ class ISATabStudyRecord:
         self.assays = []
         self.protocols = []
         self.contacts = []
+
+class NodeRecord:
+    """Represent a data node within an ISA-Tab Study/Assay file.
+    """
+    def __init__(self, name="", ntype=""):
+        self.ntype = ntype
+        self.name = name
+        self.metadata = {}
