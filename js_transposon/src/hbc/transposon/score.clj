537da9cc02208ec9dcb7dd010a1f0c1ab8a2f67c
chapmanb <chapmanb@50mail.com>
2011-10-21 10:38:47 -0400

    Normalize by row and provide summary statistics for merged, normalized data

--- js_transposon/src/hbc/transposon/score.clj
+++ js_transposon/src/hbc/transposon/score.clj
@@ -3,28 +3,83 @@
 
 (ns hbc.transposon.score
   (:use [incanter.io :only [read-dataset]])
-  (:require [incanter.core :as icore]))
+  (:require [incanter.core :as icore]
+            [incanter.stats :as stats]
+            [clojure.string :as string]
+            [fs]))
+
+;; Columns that are not experimental data
+(def ^:dynamic *ignore-cols* #{:chr :pos :seq})
+
+;; ## Dataset statistics
+
+(defn summarize-count-statistics [ds]
+  "Summarize statistics of counts in each experiment."
+  (letfn [(exp-stats [xs]
+            [(stats/mean xs) (stats/median xs) (stats/sd xs)])]
+    (map #(cons % (exp-stats (icore/sel ds :cols %)))
+         (remove #(contains? *ignore-cols* %) (icore/col-names ds)))))
+
+(defn print-count-stats [ds]
+  "Print out count statistics for the given dataset"
+  (println ("exp | mean | median | std-dev"))
+  (doseq [[x m md sd] (summarize-count-statistics ds)]
+    (println (format "%s | %.1f | %.1f | %.2f" x m md sd)))
+  ds)
+
+;; ## Dataset normalization
+;;
+;; Normalize columns by total reads and rows by percentage of maximum
+;; to allow experiment and position comparisons.
 
 (defn normalize-counts
   "Normalize counts to standard metric based on totals in each experiment.
    By default normalizes to total count in a column scaled to 1 million reads."
-  [in-file & {:keys [base ignore]
+  [dataset & {:keys [base ignore]
               :or {base 1e6
-                   ignore #{:chr :pos :seq}}}]
+                   ignore *ignore-cols*}}]
   (letfn [(normalize [x total]
             (* (/ x total) base))
           (maybe-normalize [ds col]
             (if-not (contains? ignore col)
               (icore/transform-col ds col normalize
-                                   (float (apply + (icore/$ col ds))))
+                                   (float (apply + (icore/sel ds :cols col))))
               ds))]
-    (loop [ds (read-dataset in-file :header true)
+    (loop [ds dataset
            cols (icore/col-names ds)]
       (if-let [col (first cols)]
         (recur (maybe-normalize ds col) (rest cols))
         ds))))
 
-(defn normalize-pos-ratios [ds]
+(defn- process-row [ds row f ignore]
+  "Process columns of interest in the current row"
+  (let [[same-cols proc-cols] ((juxt filter remove) #(contains? ignore %) (icore/col-names ds))]
+    (merge
+     (zipmap same-cols (map #(icore/sel ds :rows row :cols %) same-cols))
+     (zipmap proc-cols (f (map #(icore/sel ds :rows row :cols %) proc-cols))))))
+
+(defn normalize-pos-ratios
   "Normalize counts as the percentage of the max at a position.
    This normalizes by row, in contrast to normalize-counts
-   which handles columns.")
+   which handles columns."
+  [ds & {:keys [ignore]
+              :or {ignore *ignore-cols*}}]
+  (letfn [(normalize-row [row]
+            (let [cur-max (apply max row)]
+              (map #(/ % cur-max) row)))]
+    (icore/dataset (icore/col-names ds)
+                   (map #(process-row ds % normalize-row ignore)
+                        (range (icore/nrow ds))))))
+
+; ## Top level functionality
+
+(defn normalize-merge [merge-file]
+  "Normalize and prepare statistics on a merged file."
+  (-> (read-dataset merge-file :header true)
+      normalize-counts
+      normalize-pos-ratios
+      print-count-stats
+      icore/save (format "%s-normal.csv" (-> merge-file (string/split #"\.") first))))
+
+(defn -main [merge-file]
+  (normalize-merge merge-file))
--- js_transposon/test/hbc/transposon/test/score.clj
+++ js_transposon/test/hbc/transposon/test/score.clj
@@ -7,13 +7,19 @@
             [incanter.core :as icore])
   (:import [java.io StringReader]))
 
-(fact "Normalize a CSV file to reads per million counts."
-  (let [ncounts (normalize-counts "test.csv" :base 100)]
-    (icore/sel ncounts :cols :one) => [0.0 100.0]
-    (icore/sel ncounts :cols :two) => [20.0 80.0]
-    (icore/col-names ncounts) => [:chr :pos :one :two :three :seq])
-  (against-background
-    (icore/get-input-reader "test.csv") => (StringReader. (string/join "\n"
-      ["chr,pos,one,two,three,seq"
-       "chr1,10,0,50,10,GG"
-       "chr1,20,10,200,10,CC"]))))
+(let [ds (icore/dataset [:chr :pos :one :two :three :seq]
+                        [["chr1" 10  0  50 10 "GG"]
+                         ["chr1" 20 10 200 10 "CC"]])]
+  (fact "Normalize a CSV file by reads and experiment counts."
+    (let [ncounts (normalize-counts ds :base 100)
+          n2counts (normalize-pos-ratios ncounts)]
+      (icore/sel ncounts :cols :one) => [0.0 100.0]
+      (icore/sel ncounts :cols :two) => [20.0 80.0]
+      (icore/col-names ncounts) => [:chr :pos :one :two :three :seq]
+      (icore/sel n2counts :cols :one) => [0.0 1.0]
+      (icore/sel n2counts :cols :two) => [0.4 0.8]
+      (icore/col-names n2counts) => [:chr :pos :one :two :three :seq]
+      (fact "Calculate dataset statistics"
+        (let [stats (summarize-count-statistics n2counts)]
+          (count stats) => 3
+          (first stats) => (contains [:one 0.5 0.5]))))))
