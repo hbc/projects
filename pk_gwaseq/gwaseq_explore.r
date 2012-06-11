128289a19e88aba21653f22fb64b7e06552250c0
chapmanb <chapmanb@50mail.com>
2012-06-11 16:16:51 -0400

    Exploratory plots for GWASeq analysis

---
+++ pk_gwaseq/gwaseq_explore.r
@@ -0,0 +1,26 @@
+#!/usr/bin/env RScript
+
+# Exploratory plots of GWASeq combined results
+
+library(ggplot2)
+library(knitr)
+
+getProgram <- function(args) {
+  sub("--file=", "", args[grep("--file=", args)])
+}
+args <- commandArgs(trailingOnly=TRUE)
+program <- getProgram(commandArgs(trailingOnly=FALSE))
+in_file <- args[1]
+low_variant_cutoff <- 10
+qual_cutoff <- 10000
+base_name <- "gwaseq_explore"
+
+d <- read.csv(in_file, header=TRUE)
+d$total_variants <- d$HET + d$HOM_VAR
+d$is_low <- ifelse(d$total_variants < low_variant_cutoff, "low", "high")
+d$is_bait <- ifelse(d$bait == 1, "bait", "nonbait")
+d$qual_trunc <- ifelse(d$qual < qual_cutoff, d$qual, qual_cutoff)
+d.low <- subset(d, d$total_variants < low_variant_cutoff)
+
+knit(paste(file.path(dirname(program), base_name), ".Rmd", sep=""))
+system(paste("markdown2pdf ", base_name, ".md", sep=""))
