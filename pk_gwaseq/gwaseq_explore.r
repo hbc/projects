2b2946f531da31829a545c46eb74efdf1afafa9e
chapmanb <chapmanb@50mail.com>
2012-06-12 14:55:29 -0400

    Additional GWASeq plots summarizing variants

--- pk_gwaseq/gwaseq_explore.r
+++ pk_gwaseq/gwaseq_explore.r
@@ -20,7 +20,9 @@ d$total_variants <- d$HET + d$HOM_VAR
 d$is_low <- ifelse(d$total_variants < low_variant_cutoff, "low", "high")
 d$is_bait <- ifelse(d$bait == 1, "bait", "nonbait")
 d$qual_trunc <- ifelse(d$qual < qual_cutoff, d$qual, qual_cutoff)
+d$pct_hets <- d$HET / d$total_variants
 d.low <- subset(d, d$total_variants < low_variant_cutoff)
 
 knit(paste(file.path(dirname(program), base_name), ".Rmd", sep=""))
-system(paste("markdown2pdf ", base_name, ".md", sep=""))
+header <- paste(file.path(dirname(program), base_name), "-header.tex", sep="")
+system(paste("markdown2pdf -H ", header, " ", base_name, ".md", sep=""))
