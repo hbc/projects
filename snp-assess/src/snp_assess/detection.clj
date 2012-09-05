eb75cf27fd14a3829dd236289046ef024a912b1e
chapmanb <chapmanb@50mail.com>
2012-09-05 15:22:39 -0400

    Identify low-frequency background differences with sub-sampling, reporting top percentiles to set limit of detection to minimize false positives

--- snp-assess/src/snp_assess/detection.clj
+++ snp-assess/src/snp_assess/detection.clj
@@ -1,24 +1,39 @@
 (ns snp-assess.detection
   "Determine limit of detection for variant calling based on background false positives."
   (:use [clojure.java.io]
+        [incanter.stats :only [quantile]]
         [snp-assess.classify :only [prep-config-files prep-classifier-info
                                     classifier-checker read-ref
-                                    raw-reads-by-pos downsample-at-pos]]))
+                                    raw-reads-by-pos call-vrns-at-pos downsample-at-pos]]))
 
 (defn background-freqs-at-pos
   "Return background frequencies at this constant position, with downsampling."
-  [reads passes? config run-config])
+  [reads passes? config run-config]
+  (mapcat (fn [downsample]
+            (->> (call-vrns-at-pos (downsample-at-pos downsample reads) passes? config)
+                 :calls
+                 (map second)
+                 (sort >)
+                 rest))
+          (mapcat (partial repeat (get-in run-config [:downsample :replicates]))
+                  (get-in run-config [:downsample :lod-percents]))))
 
 (defn find-background-freqs
   "Determine frequency of background mutations to set cutoffs that minimize false positives"
   [data-file pos-file ref-file classifier config run-config]
   (let [passes? (classifier-checker classifier config)
-        known-vrns (read-ref pos-file ref-file 101.0)]
+        constant-is (set (->> (read-ref pos-file ref-file 101.0)
+                              (filter #(= 100.0 (second %)))
+                              (map first)
+                              (map second)))]
     (with-open [rdr (reader data-file)]
-      (->> (raw-reads-by-pos rdr config)
-           (map #(background-freqs-at-pos % passes? config run-config))))))
+      (let [probs [0.9 0.925 0.95 0.975 0.99 0.999 0.9999 1.0]
+            backgrounds (->> (raw-reads-by-pos rdr config)
+                             (filter #(contains? constant-is (:pos (first %))))
+                             (mapcat #(background-freqs-at-pos % passes? config run-config)))]
+        (zipmap probs (quantile backgrounds :probs probs))))))
 
 (defn -main [run-config-file param-config-file work-dir]
   (let [{:keys [config run-config]} (prep-config-files run-config-file param-config-file work-dir)
         {:keys [c ref-file pos-file data-file]} (prep-classifier-info run-config config work-dir)]
-    (find-background-freqs data-file pos-file ref-file c config run-config)))
+    (println (find-background-freqs data-file pos-file ref-file c config run-config))))
