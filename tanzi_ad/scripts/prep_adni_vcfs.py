84f79e23d6635e3609943945c66c4937a02ba6e0
roryk <rory.kirchner@gmail.com>
2013-11-13 21:31:47 -0500

    Script to clean and load the ADNI VCF files into GEMINI.

--- tanzi_ad/scripts/prep_adni_vcfs.py
+++ tanzi_ad/scripts/prep_adni_vcfs.py
@@ -1,12 +1,30 @@
 #!/usr/bin/env python
-#from bcbio.variation import combine_variant_files
 from argparse import ArgumentParser
 import glob
 import os
 import yaml
 from joblib import Parallel, delayed
-from bcbio.variation import vcfutils, effects, population
+import subprocess
+from itertools import dropwhile
+from six import advance_iterator
+import sys
 
+
+from bcbio.variation import vcfutils, effects, population
+from bcbio.distributed.messaging import parallel_runner
+from bcbio.distributed.transaction import file_transaction
+
+
+# def get_sample_name(vcf_file):
+#     with open(vcf_file) as in_handle:
+#         in_handle = dropwhile(lambda x: not x.startswith("#CHROM"), in_handle)
+#         l = advance_iterator(in_handle)
+#         name = l.split()[9].strip()
+#         return name.split("_")[0]
+
+def get_sample_name(vcf_file):
+    base, _ = os.path.splitext(vcf_file)
+    return os.path.basename(base.split("_SNPs")[0].upper())
 
 def prep_vcf_files(vcf_files, cores, config):
     out_files = Parallel(cores)(delayed(run_illumina_prep)(s, config) for s in vcf_files)
@@ -20,7 +38,7 @@ def merge_vcf_files(sample_files, cores, config):
     vcfutils.parallel_combine_variants(sample_files, out_file, config["ref"]["GRCh37"],
                                        config, run_parallel)
     return out_file
-    pass
+
 
 def find_vcf_files(dirname):
     vcf_extensions = ("*.vcf", "*.VCF")
@@ -30,36 +48,48 @@ def find_vcf_files(dirname):
         vcf_files.extend(glob.glob(query))
     return vcf_files
 
-def run_illumina_prep(sample, config):
+def run_illumina_prep(vcf_file, config):
     tmp_dir = config.get("tmpdir", os.getcwd())
     if not os.path.exists(tmp_dir):
         try:
             os.makedirs(tmp_dir)
         except OSError:
             assert os.path.exists(tmp_dir)
-    out_file = os.path.join(os.getcwd(), "%s.vcf" % sample["id"])
+    sample_id = get_sample_name(vcf_file)
+    out_file = os.path.join(os.getcwd(), "%s.vcf" % sample_id)
     if not os.path.exists(out_file):
-        print sample["id"], sample["dir"], out_file
+        print sample_id, vcf_file, out_file
         subprocess.check_call(["java", "-Xms1g", "-Xmx2g", "-jar", config["bcbio.variation"],
-                               "variant-utils", "illumina", sample["dir"],
-                               sample["id"], config["ref"]["GRCh37"],
+                               "variant-utils", "illumina", vcf_file,
+                               sample_id, config["ref"]["GRCh37"],
                                config["ref"]["hg19"],
                                "--outdir", os.getcwd(),
                                "--tmpdir", tmp_dir])
+    return out_file
+
+def load_gemini_db(vcf_file, ped_file, cores):
+    cmd = ("gemini load --passonly -v {vcf_file} -p {ped_file} -t snpEff --cores "
+           "{cores} {tx_gemini_db}")
+    base, _ = os.path.splitext(vcf_file)
+    gemini_db = base + ".db"
+    with file_transaction(gemini_db) as tx_gemini_db:
+        subprocess.check_call(cmd.format(**locals()), shell=True)
+    return gemini_db
 
 def main(dirname, config, cores):
     vcf_files = find_vcf_files(dirname)
     prepped_files = prep_vcf_files(vcf_files, cores, config)
     merged_file = merge_vcf_files(prepped_files, cores, config)
-    effects_file = effects.snpeff_effects({"vrn_file": merge_file,
+    effects_file = effects.snpeff_effects({"vrn_file": merged_file,
                                            "genome_resources": {"aliases" : {"snpeff": "GRCh37"}},
                                            "genome_build": "GRCh37",
                                            "config": config})
-    snpeff_file = effects.snpeff_effects(merged_file)
-    gemini_db = population.prep_gemini_db(snpeff_file,
-                                          [os.path.splitext(config["outputs"]["merge"])[0], "casava"],
-                                          [{"config": config, "work_bam": "yes", "genome_build": "GRCh37"}],
-                                          data)[0][1]["db"]
+
+    gemini_db = load_gemini_db(effects_file, config["ped"], cores)
+
+
+#    gemini_db = population.prep_gemini_db(effects_file, call_id, samples, data)[0][1]["db"]
+
 
 if __name__ == "__main__":
     parser = ArgumentParser(description="Clean, merge and load a directory of "
