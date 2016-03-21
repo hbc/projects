0246d362ad88bfd6ab824307f466b047caa1868f
chapmanb <chapmanb@50mail.com>
2016-03-21 09:28:59 +0000

    Subset analysis of combined ensemble calls by individual callers and pairs

---
+++ giab_somatic/scripts/subset_multi_caller.py
@@ -0,0 +1,36 @@
+#!/bin/env python
+"""Subset multiple callers in an input set into combinations of calls.
+
+Returns all single calls listed in callers plus all pairs.
+"""
+import itertools
+import os
+import sys
+
+from pysam import VariantFile
+
+def main(in_file, callers):
+    callers = callers.split(",")
+
+    for caller in callers:
+        subset_by_callers(in_file, [caller])
+    for c1, c2 in itertools.combinations(callers, 2):
+        subset_by_callers(in_file, [c1, c2])
+
+def subset_by_callers(in_file, callers):
+    out_file = "%s-%s.vcf.gz" % (in_file.replace(".vcf", "").replace(".gz", ""), "_".join(callers))
+    if not os.path.exists(out_file):
+        want_callers = set(callers)
+        reader = VariantFile(in_file)
+        writer = VariantFile(out_file, "w", header=reader.header)
+        count = 0
+        for rec in reader:
+            cur_callers = set(rec.info["set"].split("-"))
+            if len(cur_callers & want_callers) == len(want_callers):
+                count += 1
+                writer.write(rec)
+        print callers, count
+    return out_file
+
+if __name__ == "__main__":
+    main(*sys.argv[1:])
