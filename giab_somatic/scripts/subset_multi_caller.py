5b48d3432083b752cd31572ed436204eb0888a35
chapmanb <chapmanb@50mail.com>
2016-03-22 09:35:58 +0000

    Ensure subset files from union are bgzipped and prepare 'or' comparisons

--- giab_somatic/scripts/subset_multi_caller.py
+++ giab_somatic/scripts/subset_multi_caller.py
@@ -9,6 +9,8 @@ import sys
 
 from pysam import VariantFile
 
+from bcbio.variation import vcfutils
+
 def main(in_file, callers):
     callers = callers.split(",")
 
@@ -18,19 +20,19 @@ def main(in_file, callers):
         subset_by_callers(in_file, [c1, c2])
 
 def subset_by_callers(in_file, callers):
-    out_file = "%s-%s.vcf.gz" % (in_file.replace(".vcf", "").replace(".gz", ""), "_".join(callers))
-    if not os.path.exists(out_file):
+    out_file = "%s-%s.vcf" % (in_file.replace(".vcf", "").replace(".gz", ""), "_".join(callers))
+    if not os.path.exists(out_file) and not os.path.exists(out_file + ".gz"):
         want_callers = set(callers)
         reader = VariantFile(in_file)
         writer = VariantFile(out_file, "w", header=reader.header)
         count = 0
         for rec in reader:
             cur_callers = set(rec.info["set"].split("-"))
-            if len(cur_callers & want_callers) == len(want_callers):
+            if len(cur_callers & want_callers) > 0:
                 count += 1
                 writer.write(rec)
         print callers, count
-    return out_file
+    return vcfutils.bgzip_and_index(out_file, {})
 
 if __name__ == "__main__":
     main(*sys.argv[1:])
