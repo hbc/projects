8d245fc32f6cda148e182d2670a3f4f365815b78
John Hutchinson <jnhutchinson@gmail.com>
2016-05-06 10:29:33 -0400

    complete fdr analysis

--- shi_wgbs/scripts/shi_review.R
+++ shi_wgbs/scripts/shi_review.R
@@ -2,15 +2,19 @@ library(readr)
 library(ggvis)
 library(dplyr)
 
-setwd("/Volumes/orch/group_dir/shi_wgbs/ox_bs/work/cpg_split/s409/")
-dat <- read_delim("s409_hmc_fixed.tsv", delim="\t")
-temp <- dat[1:500000]
+setwd("~/Desktop/")
+alldat <- read_delim("s409_hmc_fixed.tsv", delim="\t")
 
+#dat <- filter(alldat, chr!="chrM")
+dat <- alldat
 
-temp %>% 
-  filter(., (C_counts+CT_counts)>=10, (ox_C_count+ox_CT_count)>=10) %>%  #coverage filter
-  rowwise() %>%  # operate on rows
-  mutate(., fisher=fisher.test(x=matrix(c(C_counts, CT_counts, ox_C_count, ox_CT_count),ncol=2), alternative="greater")$p.value)  %>% # fisher's eact test
-  select(., chr, pos, strand, C_counts, CT_counts, ox_C_count, ox_CT_count, fisher) %>%
-  mutate(., logxform=-log10(fisher)) %>%
-  compute_density(~logxform) %>% ggvis(~pred_, ~resp_) %>% layer_lines() 
+dat <- filter(dat, (CT_counts)>=10, (ox_CT_count)>=10)   #coverage filter
+dat <- rowwise(dat) %>%  mutate(., fisher=fisher.test(x=matrix(c(C_counts, (CT_counts-C_counts), ox_C_count, (ox_CT_count-ox_C_count)),ncol=2), alternative="greater")$p.value)
+dat <-   dplyr::select(dat, chr, pos, strand, C_counts, CT_counts, ox_C_count, ox_CT_count, fisher) 
+dat <- mutate(dat, ratiobs=(C_counts)/(CT_counts), ratiooxbs=(ox_C_count)/(ox_CT_count)  )
+dat <- mutate(dat, diff=ratiobs-ratiooxbs)
+dat$padjust <- p.adjust(dat$fisher, method="fdr")
+
+#randomly sample table for plotting
+ggplot(sample_n(dat,1e6), aes(x=-log10(padjust), y=CT_counts))+geom_point(alpha=0.05)
+
