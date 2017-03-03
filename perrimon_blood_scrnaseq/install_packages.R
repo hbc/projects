eb07924cef5f53b40bde08a19a999eb12f3e1b2b
Michael Steinbaugh <mike@steinbaugh.com>
2017-03-03 09:41:05 -0500

    Rename

--- perrimon_blood_scrnaseq/install_packages.R
+++
@@ -1,2 +0,0 @@
-install_url("https://github.com/hms-dbmi/scde/releases/download/1.99.1/scde_1.99.1.tgz", binary = TRUE)
-install_url("https://github.com/satijalab/seurat/releases/download/v1.4.0/Seurat_1.4.0.9.tgz", binary = TRUE)
